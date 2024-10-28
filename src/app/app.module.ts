e8bd74352ac73542cae0c0b098a5722cb16023b9
Franca Zumelli <kqp6hsjtp6@privaterelay.appleid.com>
2024-10-28 19:54:22 +0100

    fix(httpClientModule)

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,17 +1,36 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule, provideClientHydration } from '@angular/platform-browser';
-
+import { CommonModule } from '@angular/common';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ReactiveFormsModule } from '@angular/forms';
+import { FormsModule } from '@angular/forms';
+import { AuthenticationFormComponent } from './Components/authentication-form/authentication-form.component';
+import { HttpClientModule } from '@angular/common/http';
+import { SidebarComponent } from './Components/sidebar/sidebar.component';
+import { AvatarComponent } from './Components/avatar/avatar.component';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AuthenticationFormComponent,
+    SidebarComponent,
+    AvatarComponent,
+
+
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
-  ],
+    AppRoutingModule,
+    HttpClientModule,
+    CommonModule,
+    FormsModule,
+    ReactiveFormsModule
+
+
+
+],
   providers: [
     provideClientHydration()
   ],
