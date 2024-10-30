1220986a8e8b0b9078c21a86b25a3349667bbc6c
Franca Zumelli <kqp6hsjtp6@privaterelay.appleid.com>
2024-10-30 12:32:07 +0100

    update(info storage functions)

--- src/app/Services/authentication.service.ts
+++ src/app/Services/authentication.service.ts
@@ -2,51 +2,79 @@ import { formatDate, getLocaleDateFormat } from '@angular/common';
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { DeferBlockBehavior } from '@angular/core/testing';
-import { AuthResponse } from '../Interfaces/interface';
+import { AuthResponse, User } from '../Interfaces/interface';
 import { Observable } from 'rxjs';
+import { Router } from '@angular/router';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class AuthenticationService {
+  constructor(private http: HttpClient, private router: Router) {}
 
+  serverUrl: string = 'http://localhost:8080';
 
-  constructor(
-    private http: HttpClient
-  ) { }
-
-  serverUrl: string = "http://localhost:8080"
-
-  login(email: string, password: string): Observable<any>{
+  login(email: string, password: string): Observable<any> {
     const requestBody = {
       email: email,
-      password: password
-    }
-    return this.http.post(`${this.serverUrl}/api/v1/auth/login`, requestBody)
-
+      password: password,
+    };
+    return this.http.post(`${this.serverUrl}/api/v1/auth/login`, requestBody);
   }
-  
-  signup(name: string, surname: string, email: string, password: string, birthdate: Date): Observable<any>{
+
+  signup(
+    name: string,
+    surname: string,
+    email: string,
+    password: string,
+    birthdate: Date
+  ): Observable<any> {
     const requestBody = {
       name: name,
       surname: surname,
       email: email,
       password: password,
-      birthdate: formatDate(birthdate, "yyyy-MM-dd", "en-US")
+      birthdate: formatDate(birthdate, 'yyyy-MM-dd', 'en-US'),
+    };
+    console.log(requestBody);
 
+    return this.http.post(`${this.serverUrl}/api/v1/auth/signup`, requestBody);
+  }
+
+  storeToken(token: string) {
+    if (typeof localStorage !== 'undefined') {
+      // TODO to cypher
+      localStorage.setItem('token', token);
+      this.router.navigate(['homepage']);
     }
-    console.log(requestBody)
+  }
 
-    return this.http.post(`${this.serverUrl}/api/v1/auth/signup`, requestBody)
-      
+  get token() {
+    if (typeof localStorage !== 'undefined') {
+      const token = localStorage.getItem('token');
+      return token ? token : null;
+    }
+    return null;
+  }
+
+  storeUserInformation(user: any) {
+    if (typeof localStorage !== 'undefined') {
+      localStorage.setItem('user', JSON.stringify(user));
+    }
+  }
+
+  get userInformation() {
+    if (typeof localStorage !== 'undefined') {
+      const user = localStorage.getItem('user');
+      return user ? JSON.parse(user) : null;
+    }
+    return null;
   }
 
 
-  storeToken(token: string){
-    // TODO to cypher
-    localStorage.setItem("token", token)
+  logOut(){
+    localStorage.removeItem("token")
+    localStorage.removeItem("user")
+    this.router.navigate(['authentication'])
   }
-
-
-
 }
