f686c1c93cb85d943373660bec666ffb1e5df082
Franca Zumelli <kqp6hsjtp6@privaterelay.appleid.com>
2024-10-30 12:31:35 +0100

    update(dynamic menulist)

--- src/app/Components/sidebar/sidebar.component.ts
+++ src/app/Components/sidebar/sidebar.component.ts
@@ -1,4 +1,8 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { DatabaseService } from '../../Services/database.service';
+import { AuthenticationService } from '../../Services/authentication.service';
+import { Menu } from '../../Interfaces/interface';
+import { Router, Routes } from '@angular/router';
 
 @Component({
   selector: 'app-sidebar',
@@ -7,4 +11,30 @@ import { Component } from '@angular/core';
 })
 export class SidebarComponent {
 
+  constructor(
+    private dbService: DatabaseService,
+    private authService: AuthenticationService,
+    private router: Router
+  ){
+    this.getUserInfo()
+  }
+
+  menuList: Menu[] = []
+  userName: string = ""
+  userSurname: string = ""
+
+  
+  getUserInfo(){
+    this.userName = this.authService.userInformation.name
+    this.userSurname = this.authService.userInformation.surname
+    this.menuList = JSON.parse(this.authService.userInformation.menuList)
+    console.log(this.menuList)
+  }
+  isActive(link: string): boolean {
+    return this.router.isActive(link, true); 
+  }
+
+  logOut(){
+    this.authService.logOut()
+  }
 }
