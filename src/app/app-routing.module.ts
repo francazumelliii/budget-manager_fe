2973c4f0ec9667545c53d8faa3ff64d5f7c21efa
Franca Zumelli <kqp6hsjtp6@privaterelay.appleid.com>
2024-10-30 12:30:26 +0100

    update(child routes for sidebar )

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,14 @@
-import { NgModule } from '@angular/core';
+import { NgModule, Component } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AuthenticationFormComponent } from './Components/authentication-form/authentication-form.component';
+import { SidebarComponent } from './Components/sidebar/sidebar.component';
+import { HomepageComponent } from './Components/homepage/homepage.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: "", component: AuthenticationFormComponent},
+  {path: "homepage", component: SidebarComponent,children: [{ path: '', component: HomepageComponent }]},
+  {path: "authentication", component: AuthenticationFormComponent},
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
