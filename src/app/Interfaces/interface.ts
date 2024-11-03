e3d883ebe8ae1a19b917539e8616e9725088d5bf
Franca Zumelli <kqp6hsjtp6@privaterelay.appleid.com>
2024-11-03 12:38:56 +0100

    new(intefaces)

--- src/app/Interfaces/interface.ts
+++ src/app/Interfaces/interface.ts
@@ -1,3 +1,5 @@
+import { Timestamp } from "rxjs"
+
 export interface Interface {
 }
 
@@ -15,11 +17,63 @@ export interface User{
     image: string, 
     children: User[] | null,
     parent: User | null
+    role: Role
 }
-
+export enum Role{
+    PARENT,
+    USER
+}
 
 export interface Menu{
     name: string,
     icon: string,
     value: any
+}
+
+export interface Expense{
+    id: number,
+    name: string,
+    description: string, 
+    amount: number,
+    frequency: string, 
+    createdAt: string, 
+    date: string,
+    image: string, 
+    category: Category,
+    projectId: number
+}
+export interface Category{
+    id: number,
+    name: string,
+    description: string, 
+    image: string
+}
+
+export interface Income{
+    id: number, 
+    name: string, 
+    description: string, 
+    amount: number ,
+    image: string, 
+    createdAt: string, 
+    date: string
+}
+
+export interface Project{
+    id: number,
+    name: string, 
+    description: string, 
+    image: string,
+    goalAmount: string,
+    expenses: Expense[]
+    createdAt: string, 
+    creator: SimpleAccount,
+    accounts: SimpleAccount[]
+}
+
+export interface SimpleAccount{
+    name: string,
+    surname: string, 
+    email: string, 
+    image: string
 }
