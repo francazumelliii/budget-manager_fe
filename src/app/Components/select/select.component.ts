4cf69bb706a35f2ac20e16fd91d181166f27a581
Franca Zumelli <kqp6hsjtp6@privaterelay.appleid.com>
2024-11-05 09:46:13 +0100

    update(added form control inputs)

--- src/app/Components/select/select.component.ts
+++ src/app/Components/select/select.component.ts
@@ -1,12 +1,17 @@
 import { Component, Input } from '@angular/core';
+import { AbstractControl, FormControl, FormControlName, FormGroup } from '@angular/forms';
 
 @Component({
   selector: 'app-select',
   templateUrl: './select.component.html',
   styleUrl: './select.component.sass'
 })
-export class SelectComponent {
-  @Input() list: any[] = []
-  @Input() label: string = "Select an option.."
-
+export class SelectComponent   
+ {
+  @Input() list: any[] = [];
+  @Input() label: string = "Select an option..";
+  @Input() control!: AbstractControl;
+  get ctrl() {
+    return this.control as FormControl;
+  }
 }
