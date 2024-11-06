4de5bce434b49089e87bbbc8fe6a6a73bc3041b5
Franca Zumelli <kqp6hsjtp6@privaterelay.appleid.com>
2024-11-06 17:36:59 +0100

    new(profile component route)

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,10 +3,12 @@ import { RouterModule, Routes } from '@angular/router';
 import { AuthenticationFormComponent } from './Components/authentication-form/authentication-form.component';
 import { SidebarComponent } from './Components/sidebar/sidebar.component';
 import { HomepageComponent } from './Components/homepage/homepage.component';
+import { ProfileComponent } from './Components/profile/profile.component';
 
 const routes: Routes = [
   {path: "", component: AuthenticationFormComponent},
   {path: "homepage", component: SidebarComponent,children: [{ path: '', component: HomepageComponent }]},
+  {path: "profile", component: SidebarComponent,children: [{ path: '', component: ProfileComponent }]},
   {path: "authentication", component: AuthenticationFormComponent},
 ];
 
