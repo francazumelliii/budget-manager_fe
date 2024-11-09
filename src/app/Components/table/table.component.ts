8a55fe7711d52deb39f687232c5922df9a902fc3
Franca Zumelli <kqp6hsjtp6@privaterelay.appleid.com>
2024-11-09 19:19:26 +0100

    new(table component with paginator)

---
+++ src/app/Components/table/table.component.ts
@@ -0,0 +1,25 @@
+import { Component, Input, Output, EventEmitter } from '@angular/core';
+import { PaginatorState } from 'primeng/paginator';
+import { Pagination } from '../../Interfaces/interface';
+
+
+@Component({
+  selector: 'app-table',
+  templateUrl: './table.component.html',
+  styleUrl: './table.component.sass',
+})
+export class TableComponent {
+  @Input() showPaginator: boolean = true;
+  @Output() pageChange = new EventEmitter<PaginatorState>()
+  @Input() pagination: Pagination = {
+    rows: 10,
+    first: 0,
+    rowsPerPageOptions: [10,15,20],
+    totalRecords: 100
+  }
+
+  onPageChange(event: PaginatorState) {
+    console.log(event)
+    this.pageChange.emit(event)
+  }
+}
