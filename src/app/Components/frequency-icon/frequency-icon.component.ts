7ce7286cf9fe6a971c7d931789f92da53ed2ceef
Franca Zumelli <kqp6hsjtp6@privaterelay.appleid.com>
2024-11-10 12:45:49 +0100

    new(frequency icon component)

---
+++ src/app/Components/frequency-icon/frequency-icon.component.ts
@@ -0,0 +1,24 @@
+import { Component, Input } from '@angular/core';
+
+@Component({
+  selector: 'app-frequency-icon',
+  templateUrl: './frequency-icon.component.html',
+  styleUrl: './frequency-icon.component.sass'
+})
+export class FrequencyIconComponent {
+  @Input() frequency: string = "";
+  
+  frequencyString: string = "";
+  ngOnInit(): void {
+    switch(this.frequency){
+      case "S": this.frequencyString = "Single"
+        break;
+      case "W": this.frequencyString = "Weekly"
+        break;
+      case "M": this.frequencyString = "Monthly"
+        break;
+      case "Y": this.frequencyString = "Annual"
+    }
+
+  }
+}
