072bfafd6471194ebe0efbe68c432fa40ce98c06
Franca Zumelli <kqp6hsjtp6@privaterelay.appleid.com>
2024-11-17 11:04:51 +0100

    fix(dynamic avatars based on username)

--- src/app/Components/avatar/avatar.component.ts
+++ src/app/Components/avatar/avatar.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
 
 @Component({
   selector: 'app-avatar',
@@ -6,5 +6,11 @@ import { Component } from '@angular/core';
   styleUrl: './avatar.component.sass'
 })
 export class AvatarComponent {
-
+  @Input() name: string = "User User"
+  @Input() type: string = "pixel-art"
+  
+  generateAvatar(name: string ,type :string){
+    return `https://api.dicebear.com/6.x/${type}/svg?seed=${encodeURIComponent(name)}`
+  }
+  
 }
