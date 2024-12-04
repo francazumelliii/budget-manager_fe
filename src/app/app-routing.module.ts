2c7aae85a4832f726018ddffc7ba68a69126004b
Franca Zumelli <kqp6hsjtp6@privaterelay.appleid.com>
2024-12-04 15:42:44 +0100

    new(authGuard for routes)

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -12,18 +12,19 @@ import { ChildComponent } from './Components/child/child.component';
 import { AccountComponent } from './Components/account/account.component';
 import { ProjectComponent } from './Components/project/project.component';
 import { SingleProjectComponent } from './Components/single-project/single-project.component';
+import { authGuard } from './Guards/auth.guard';
 
 const routes: Routes = [
-  {path: "", component: AuthenticationFormComponent},
-  {path: "homepage", component: SidebarComponent,children: [{ path: '', component: HomepageComponent }]},
-  {path: "profile", component: SidebarComponent,children: [{ path: '', component: ProfileComponent }]},
-  {path: "expenses", component: SidebarComponent,children: [{ path: '', component: ExpenseComponent }]},
-  {path: "incomes", component: SidebarComponent,children: [{ path: '', component: IncomeComponent }]},
-  {path: "account", component: SidebarComponent,children: [{ path: '', component: AccountComponent }]},
-  {path: "projects", component: SidebarComponent,children: [{ path: '', component: ProjectComponent }]},
-  {path: "projects/:id", component: SidebarComponent,children: [{ path: '', component: SingleProjectComponent }]},
-  {path: "children", component: SidebarComponent,children: [{ path: '', component: ChildrenComponent }]},
-  {path: "children/:id", component: SidebarComponent,children: [{ path: '', component: ChildComponent }]},
+  {path: "", pathMatch: "full",  component: AuthenticationFormComponent},
+  {path: "homepage", component: SidebarComponent,children: [{ path: '', component: HomepageComponent }], canActivate: [authGuard]},
+  {path: "profile", component: SidebarComponent,children: [{ path: '', component: ProfileComponent }], canActivate: [authGuard]},
+  {path: "expenses", component: SidebarComponent,children: [{ path: '', component: ExpenseComponent }], canActivate: [authGuard]},
+  {path: "incomes", component: SidebarComponent,children: [{ path: '', component: IncomeComponent }], canActivate: [authGuard]},
+  {path: "account", component: SidebarComponent,children: [{ path: '', component: AccountComponent }], canActivate: [authGuard]},
+  {path: "projects", component: SidebarComponent,children: [{ path: '', component: ProjectComponent }], canActivate: [authGuard]},
+  {path: "projects/:id", component: SidebarComponent,children: [{ path: '', component: SingleProjectComponent }], canActivate: [authGuard]},
+  {path: "children", component: SidebarComponent,children: [{ path: '', component: ChildrenComponent }], canActivate: [authGuard]},
+  {path: "children/:id", component: SidebarComponent,children: [{ path: '', component: ChildComponent }], canActivate: [authGuard]},
   {path: "authentication", component: AuthenticationFormComponent},
 ];
 
